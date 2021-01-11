7c2d0f90901f9e5521c33c62a0f4a6162c8f7927
MatheuSilvSant <matheu.lpp@gmail.com>
2021-01-11 19:40:39 -0300

    Project created

---
+++ backend/src/test/java/com/clone/netflix/NetflixcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.netflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
