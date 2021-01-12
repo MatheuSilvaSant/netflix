63146248c4d29550cea27c7964b333ef33818b6b
MatheuSilvSant <matheu.lpp@gmail.com>
2021-01-12 01:01:29 -0300

    Created domain and Association domain (Usuario,Conta,Filme,Categoria)

---
+++ backend/src/main/java/com/clone/netflix/domain/Usuario.java
@@ -0,0 +1,75 @@
+package com.clone.netflix.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Long id;
+	private String nome;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Conta> contas = new ArrayList<>();
+	
+	public Usuario() {}
+
+	public Usuario(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
