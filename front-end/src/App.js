07895214b564629427006fe33dab9c0f79c2cd1c
MatheuSilvSant <matheu.lpp@gmail.com>
2021-01-17 17:30:47 -0300

    Finish Header and footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,15 +2,16 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import Tmdb from './Tmdb';
-import MovieRow from './components/MovieRow/index';
-import FeaturedMovie from './components/FeaturedMovie/index';
-
+import MovieRow from './components/MovieRow/';
+import FeaturedMovie from './components/FeaturedMovie/';
+import Header from './components/Header/';
 
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(()=>{
     const loadAll = async () =>{
@@ -28,8 +29,26 @@ export default () => {
     return loadAll();
 },[]);
 
+  useEffect(()=>{
+      const scrollListener = () =>{
+          if(window.scrollY > 10){
+            setBlackHeader(true);
+          }else{
+            setBlackHeader(false);
+          }
+      }
+      window.addEventListener('scroll', scrollListener);
+
+      return () => {
+        window.removeEventListener('scroll', scrollListener);
+      }
+  },[])
+
+
   return (
     <div className="page">
+
+    <Header black={blackHeader}/>
 
         {featuredData &&
           <FeaturedMovie item={featuredData}/>
@@ -41,6 +60,13 @@ export default () => {
 
         ))}
       </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">💖 </span>
+        pelo Matheus com a aula do Bonieky da <a href="https://b7web.com.br/fullstack" >b7web.</a><br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site <a href="https://www.themoviedb.org/?language=pt-BR" >Themoviedb</a>
+      </footer>
     </div>
   );
 }
